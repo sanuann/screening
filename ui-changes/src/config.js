8eae72ef357400dc930af32cabd8fb545856da15
Sanu Ann <sanuann@mit.edu>
2020-08-05 02:46:53 -0400

    add screening protocol

---
+++ ui-changes/src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  /* eslint-disable */
+  githubSrc: 'https://raw.githubusercontent.com/mentalwellbeing/screening/master/protocol/MentalWellBeingScreening_schema',
+  banner: 'This is a screening protocol for Mental Health Wellbeing study.',
+  startButton: 'Join',
+  assetsPublicPath: '/screening/',
+  backendServer: null
+};
